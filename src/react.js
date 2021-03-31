3db543182875f619ccb258b5195c7ee3d4bfc17b
shanto140319 <shanto140319@gmail.com>
2021-03-31 10:56:32 +0600

    new project added

--- src/react.js
+++ src/react.js
@@ -1,122 +1,128 @@
 export const reactbasic = [
-{
+  {
     id: 1,
     title: 'Cricket Team',
     category: '',
     img: '../images/cricket.PNG',
-    link:'https://cricket-player.netlify.app/',
- },
- {
+    link: 'https://cricket-player.netlify.app/',
+  },
+  {
     id: 2,
     title: 'Reviews',
     category: '',
     img: '../images/review.PNG',
-    link:'https://react-reviews-setup.netlify.app/',
- },
- {
+    link: 'https://react-reviews-setup.netlify.app/',
+  },
+  {
     id: 3,
     title: 'Accordion',
     category: '',
     img: '../images/accordion.PNG',
-    link:'https://react-accordions.netlify.app/',
- },
+    link: 'https://react-accordions.netlify.app/',
+  },
   {
     id: 4,
     title: 'tabs',
     category: '',
     img: '../images/tabs.PNG',
-    link:'https://react-tab.netlify.app/',
- },
+    link: 'https://react-tab.netlify.app/',
+  },
   {
     id: 5,
     title: 'sidebar',
     category: '',
     img: '../images/sidebar.PNG',
-    link:'https://react-sidebars.netlify.app/',
- },
+    link: 'https://react-sidebars.netlify.app/',
+  },
 
   {
     id: 6,
     title: 'sports mania',
     category: '',
     img: '../images/sports.PNG',
-    link:'https://sport-mania.netlify.app',
- },
- 
+    link: 'https://sport-mania.netlify.app',
+  },
 ]
 
 export const reactAdvance = [
-    {
+  {
     id: 1,
     title: 'menu',
     category: '',
     img: '../images/menu.PNG',
-    link:'https://react-menus.netlify.app/',
- },
- {
+    link: 'https://react-menus.netlify.app/',
+  },
+  {
     id: 2,
     title: 'lorem ipsum',
     category: '',
     img: '../images/lorem.PNG',
-    link:'https://react-lorem-generate.netlify.app/',
- },
- {
+    link: 'https://react-lorem-generate.netlify.app/',
+  },
+  {
     id: 3,
     title: 'submenu',
     category: '',
     img: '../images/stripe.PNG',
-    link:'https://react-submenu.netlify.app/',
- },
- {
+    link: 'https://react-submenu.netlify.app/',
+  },
+  {
     id: 4,
     title: 'coktails',
     category: '',
     img: '../images/coktail.PNG',
-    link:'https://react-coktail.netlify.app/',
- },
+    link: 'https://react-coktail.netlify.app/',
+  },
   {
     id: 5,
     title: 'random person',
     category: '',
     img: '../images/random.PNG',
-    link:'https://react-random-person.netlify.app/',
- },
+    link: 'https://react-random-person.netlify.app/',
+  },
   {
     id: 6,
     title: 'pagination',
     category: '',
     img: '../images/pagination.PNG',
-    link:'https://react-paginations.netlify.app/',
- },
-{
+    link: 'https://react-paginations.netlify.app/',
+  },
+  {
     id: 7,
     title: 'movies db',
     category: '',
     img: '../images/movie.PNG',
-    link:'https://movie-db-test.netlify.app',
- },
+    link: 'https://movie-db-test.netlify.app',
+  },
   {
     id: 8,
     title: 'Hacker news',
     category: '',
     img: '../images/hacker.PNG',
-    link:'https://hacker-news-test.netlify.app/',
- },
+    link: 'https://hacker-news-test.netlify.app/',
+  },
   {
     id: 9,
     title: 'unsplash db',
     category: '',
     img: '../images/unsplash.PNG',
-    link:'https://unsplash-clone-in.netlify.app',
- },
+    link: 'https://unsplash-clone-in.netlify.app',
+  },
 ]
 
- export const reactlarge = [
- {
+export const reactlarge = [
+  {
     id: 1,
+    title: 'Covid 19 Live',
+    category: '',
+    img: '../images/covid.PNG',
+    link: 'https://covid-tracker-live.netlify.app/',
+  },
+  {
+    id: 2,
     title: 'Ecommerce website',
     category: '',
     img: '../images/comfy.PNG',
-    link:'https://ecommerce-comfy.netlify.app/',
- },
+    link: 'https://ecommerce-comfy.netlify.app/',
+  },
 ]
